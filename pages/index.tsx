1c2e87ec1b3052e3c56cc17ccadd15d0b036fda8
Ivan Portugal <ivanbportugal@gmail.com>
2023-01-17 22:49:33 -0600

    another stop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,106 +1,45 @@
 import Head from 'next/head';
-import NextLink from 'next/link'
-import { Avatar, Box, Button, Link, List, ListItem, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Text, useToast, Wrap } from '@chakra-ui/react'
 import styles from '../styles/Home.module.css';
-import { getChapters } from '../lib/get-json';
-import { getBookMap } from '../lib/get-book-descriptions';
-import { getLastVisited } from '../lib/local-data';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { Positioner } from '../components/positioner';
 
 export default function Home({ data = {} }) {
 
-  const allBookChapters: Array<any> = data as any;
-  const toast = useToast();
-  const router = useRouter();
-
-  const renderBooks = allBookChapters.map(({ bookName }) => {
-    const url = `/book/${bookName}`;
-    const bookMeta = getBookMap(bookName);
-    return <ListItem key={bookName}>
-      <Wrap className={styles.booklist}>
-        <Popover>
-          <PopoverTrigger>
-            <Avatar name={bookMeta.category} />
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverHeader fontWeight='semibold'>{bookMeta.category}</PopoverHeader>
-            <PopoverBody>
-              {bookMeta.author} - {bookMeta.date}
-            </PopoverBody>
-          </PopoverContent>
-          <Link as={NextLink} href={url} passHref>
-            {bookName}
-          </Link>
-        </Popover>
-      </Wrap>
-    </ListItem>
-  });
-
-  useEffect(() => {
-    getLastVisited().then(lastVisited => {
-      if (lastVisited) {
-        toast({
-          status: 'info',
-          duration: 3000,
-          isClosable: true,
-          position: 'bottom-right',
-          render: () => (
-            <Box color='white' p={3} bg='blue.500'>
-              <Text fontSize='md'>Pick up where you left off?</Text>
-              <Text fontSize='sm'>({lastVisited})</Text>
-              <Button size='sm' onClick={() => {
-                router.push(lastVisited);
-                toast.closeAll();
-              }}>{'Let\'s Go!'}</Button>
-            </Box>
-          ),
-        })
-      }
-    })
-  }, [router, toast]);
 
   return (
     <div className={styles.container}>
       <Head>
-        <title>The ESV Bible</title>
-        <meta name="description" content="The ESV translation of the Bible. Just the text, no study notes or anything." />
+        <title>Find Me!</title>
+        <meta name="description" content="Share your location to a friend" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Simple access to awesome text.
+          Fry Me?! No, FIND me.
         </h1>
 
         <div className={styles.description}>
           <div className={styles.card}>
-            <p style={{fontWeight: 'bold'}}>FAQs</p>
+            <p style={{fontWeight: 'bold'}}>Instructions</p>
             <ul className={styles.stylelinks}>
               <li>
-                <p style={{textDecoration: 'underline'}}>Why another bible app?</p>
-                <p>Because I felt like it.</p>
+                <p style={{textDecoration: 'underline'}}>Step 1</p>
+                <p>Track your location</p>
               </li>
               <li>
-                <p style={{textDecoration: 'underline'}}>Ok but, why should I use yours and not some other one?</p>
-                <p>No app to download. Can be installed on your home screen. No account is needed. Searches are done on the browser.</p>
+                <p style={{textDecoration: 'underline'}}>Step 2</p>
+                <p>Share it to a friend</p>
               </li>
               <li>
-                <p style={{textDecoration: 'underline'}}>Do you have any study material?</p>
-                <p>No, but there are several neat resources online. Start here: <a target='_blank' rel="noreferrer" href='https://bible.org'>Bible.org</a></p>
+                <p style={{textDecoration: 'underline'}}>Step 3</p>
+                <p>Your friend uses the link to find you.</p>
               </li>
               <li>
                 <p style={{textDecoration: 'underline'}}>Do you want money for your work?</p>
                 <p>No, but you can always <a target='_blank' rel="noreferrer" href='https://buymeacoffee.com/ivanportugal'>Buy Me a Coffee</a></p>
               </li>
               <li>
-                <p style={{textDecoration: 'underline'}}>Do you track my usage?</p>
-                <p>Nothing other than standard Google Analytics to see if people are actually using this.</p>
-              </li>
-              <li>
-                <p style={{textDecoration: 'underline'}}>Which translations do you support</p>
-                <p>Only <a target='_blank' rel="noreferrer" href='https://www.esv.org/'>ESV</a> for now.</p>
+                <p style={{textDecoration: 'underline'}}>Do you track me?</p>
+                <p>Nope. Sharing is done from your device to theirs. I know nothing.</p>
               </li>
               <li>
                 <p style={{textDecoration: 'underline'}}>I have a feature request and / or see a bug. (Or I just have more questions and comments)</p>
@@ -112,31 +51,13 @@ export default function Home({ data = {} }) {
         </div>
 
         <hr />
-
-        <List spacing={5} className={styles.homelist}>
-          {renderBooks}
-        </List>
-
+        <Positioner />
+        
       </main>
 
       <footer className={styles.footer}>
-          <span className={styles.stylelinks}>Powered by the word of God (and <a target='_blank' rel="noreferrer" href='https://buymeacoffee.com/ivanportugal'>coffee</a>).</span>
+          <span className={styles.stylelinks}>Powered by <a target='_blank' rel="noreferrer" href='https://buymeacoffee.com/ivanportugal'>coffee</a>.</span>
       </footer>
     </div>
   )
-}
-
-// Keep __NEXT_DATA__ hydrated with the text no matter which landing page
-export async function getStaticProps() {
-  const { thebooks } = await getChapters();
-  const onlyNames = thebooks.map(book => {
-    return {
-      bookName: book.bookName
-    }
-  })
-  return {
-    props: {
-      data: onlyNames
-    }
-  }
 }
