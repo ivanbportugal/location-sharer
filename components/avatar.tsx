eca2d20faea52a0d52c8569b176494356876c38c
Ivan Portugal <ivanbportugal@gmail.com>
2023-01-28 14:17:36 -0600

    calculator avatar

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -1,18 +1,35 @@
 import styles from './avatar.module.css'
 import bg from '../public/avatarians.jpg'
 import { Navbar } from '@nextui-org/react'
+import { useState } from 'react'
 
-const Avatar = () => {
+interface AvatarPosition {
+  x: number,
+  y: number
+}
+
+const Avatar = (position: AvatarPosition) => {
 
   // X: -6px. -> -38px -> -73 -> 109 ~= 35px apart. 25 columns
   // Y: -9px -> -45px -> -79 -> -115. ~= 35px apart. 4 rows.
+  const calculatePixels = () => {
+    const valueX = -((position.x * 35) + 6)
+    const valueY = -((position.y * 35) + 9)
+    setThePosition({
+      x: valueX + 'px',
+      y: valueY + 'px'
+    })
+  }
+
+  const [thePosition, setThePosition] = useState<any>(calculatePixels())
+
 
   const custom = <span
     className={styles.avatar}
     style={{
       backgroundImage: `url(${bg.src})`,
-      backgroundPositionX: '20px',
-      backgroundPositionY: '60px'
+      backgroundPositionX: thePosition.x,
+      backgroundPositionY: thePosition.y
     }}
    />
 
